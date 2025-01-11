5440db0aedc1fc2cbc240d0b5048e65d4ae002b5
meicib <ameicib@berkeley.edu>
2025-01-11 14:04:11 -0800

    Added art

--- scripts.js
+++ scripts.js
@@ -44,7 +44,7 @@ function quiz() {
     const questions = [
         {
             question: "question 1",
-            image: "assets/transparent-hyrax.webp",
+            image: "assets/scenes/scene1.png",
             text: "It’s a cool, autumn afternoon, and you’ve decided to visit your local aquarium with your friends. You’re sitting in front of a particularly luminous tank with various jellyfish propelling themselves through the water. Transfixed by their rhythmic movements, your eyelids start to droop, the darkness of the aquarium fueling your sleepiness when suddenly– SPLASH! You’ve somehow been dropped right into the cold waters of the jellyfish exhibit. What do you do?",
             choices: [
                 "Panic! What if the aquarium staff kick you out because they thought you jumped in?", 
@@ -56,7 +56,7 @@ function quiz() {
         },
         {
             question: "question 2a",
-            image: "assets/transparent-elephant-shrew.webp",
+            image: "assets/scenes/scene2a.png",
             text: "Despite the commotion you make, no one else seems to notice your predicament. More and more jellyfish begin to surround you, until your vision is obscured by a kaleidoscope of vivid colors.",
             choices: [
                 "You swipe at the jellyfish, hoping to clear them away.",
@@ -69,7 +69,7 @@ function quiz() {
         },
         {
             question: "question 2b",
-            image: "assets/transparent-manatee.webp",
+            image: "assets/scenes/scene2b.png",
             text: "The water wraps around your limbs with no end in sight, just an endless void of darkness beyond the glow of the jellyfish. Soon, your view of the top of the tank is shrouded by a mass of jellyfish surrounding you.",
             choices: [
                 "You simply gaze at the whirling colors, appreciating its beauty.",
@@ -82,7 +82,7 @@ function quiz() {
         },
         {
             question: "question 3",
-            image: "assets/transparent-elephant-shrew.webp",
+            image: "assets/scenes/scene3.png",
             text: "You squeeze your eyes shut once the colors become too intense. When you open them again, you find yourself in a bustling, underwater market. Where everyone is a jellyfish, including you it seems. What’s your plan of action?",
             choices: [
                 "Talk to the locals!",
@@ -95,7 +95,7 @@ function quiz() {
         },
         {
             question: "question 4a",
-            image: "assets/transparent-manatee.webp",
+            image: "assets/scenes/scene4a.png",
             text: "The jellyfish around you don’t seem to mind your unfamiliarity with your new form and environment. Rather, they encourage you to make the most out of your stay and inform you that should you choose to return to your world, you would have to visit the king.",
             choices: [
                 "You thank them for their kindness and decide to make your way to the palace ",
@@ -108,7 +108,7 @@ function quiz() {
         },
         {
             question: "question 4b",
-            image: "assets/transparent-elephant-shrew.webp",
+            image: "assets/scenes/scene4b.png",
             text: "As you make your way through the vibrant market, several jellyfish notice your alert, seemingly puzzled gaze. They are perceptive of your novelty in this world, and offer to guide you to the king, who can help you return to the human realm.",
             choices: [
                 "You head towards the palace, hoping the king doesn’t misunderstand your intentions.",
@@ -121,7 +121,7 @@ function quiz() {
         },
         {
             question: "question 5",
-            image: "assets/transparent-manatee.webp",
+            image: "assets/scenes/scene5.png",
             text: "The palace looms largely in front of you, an imposing structure made from colorful coral. Two guards stand by the entrance, but it seems as though your arrival was expected, as they push the tall doors open, motioning for you to enter. How do you make your entrance?",
             choices: [
                 "You make a grand first impression! An extravagant entrance, by an important person (or, jellyfish right now), for a big problem, right?",
@@ -134,7 +134,7 @@ function quiz() {
         },
         {
             question: "question 6a",
-            image: "assets/transparent-elephant-shrew.webp",
+            image: "assets/scenes/scene6.png",
             text: "The king flutters his filaments at your entrance, finding your grandiose actions amusing. He seems to understand your efforts convey the importance of your situation, and he motions for you to follow him, leading you into a dark side room.",
             choices: [
                 "Jokingly ask if you should be afraid for your safety.",
@@ -147,7 +147,7 @@ function quiz() {
         },
         {
             question: "question 6b",
-            image: "assets/transparent-manatee.webp",
+            image: "assets/scenes/scene6.png",
             text: "The queen in particular seems sympathetic towards your plight, nudging the king forward with her tendrils. He approaches you and gently indicates for you to follow, bringing you to a dimly lit side room.",
             choices: [
                 "Position yourself near the door in case you need to run.",
@@ -160,7 +160,7 @@ function quiz() {
         },
         {
             question: "question 7",
-            image: "assets/transparent-elephant-shrew.webp",
+            image: "assets/scenes/scene7.png",
             text: "The king seems to realize the sketchiness of the situation, and quickly explains to you that he hails from the deep sea, so he prefers little light. Before you can respond, the king starts to wave his tentacles. A bright glow emanates from them, filling up the room until it’s too much for you to handle. Instinctively, you close your eyes. Wait, your eyes?",
             choices: [
                 "Wiggle your limbs. Do they feel like tendrils, or hands and fingers?",
@@ -173,7 +173,7 @@ function quiz() {
         },
         {
             question: "question 8a",
-            image: "assets/transparent-manatee.webp",
+            image: "assets/scenes/scene8a.png",
             text: "Upon opening your eyes, you find that you are still on that same bench you fell asleep on. The aquarium lights are as dim as ever, and the jellyfish float in their tank as if nothing had ever happened. You can hear the sound of footsteps, the familiar chatter of your friends’ voices growing louder.",
             choices: [
                 "You watch the jellyfish quietly while waiting for your friends to arrive, wondering what the experience meant.",
@@ -186,7 +186,7 @@ function quiz() {
         },
         {
             question: "question 8b",
-            image: "assets/transparent-elephant-shrew.webp",
+            image: "assets/scenes/scene8b.png",
             text: "You notice almost immediately that you are right where you fell asleep: in the dimly lit room, on that hard bench, with the jellyfish in their tank. You hear the chatter of your friends in the distance, somewhere else in the aquarium.",
             choices: [
                 "Keep quiet about the incident, wanting to figure out the phenomenon yourself before involving others.",
@@ -201,48 +201,56 @@ function quiz() {
 
     const jellyinfo = {
         "Fried Egg Jelly": {
+            img: "assets/jellyfish/fried-egg.png",
             traits: "Laidback, Easygoing, Go-with-the-flow",
             habitat: "You enjoy living in shallow sea depths, letting the waves of the seas propel you. A favorite activity of yours is letting the sun, shining through the crystalline water, warm your body.",
             compatible: "Black Sea Nettle, Spotted Jelly, Moon Jelly",
             incompatible: "Man-O-War, Elegant Jelly"
         },
         "Man-O-War": {
+            img: "assets/jellyfish/man-o-war.png",
             traits: "Assertive, Tough, Adamant",
             habitat: "You enjoy traversing through the surface of the sea or beaches, never afraid to battle strong waves or the hands of curious humans. Maybe you should work on alleviating your painful sting!",
             compatible: "Elegant Jelly, Black Sea Nettle",
             incompatible: "Flower Hat Jelly, Moon Jelly"
         },
         "Black Sea Nettle": {
+            img: "assets/jellyfish/black-sea-nettle.png",
             traits: "Outgoing, Playful, Goofy",
             habitat: "You live in coastal waters, enjoying the liveliness of the teeming habitats. An ordinary day consists of cheerfully greeting crabs, manta rays, sardines, sea lions, and all the other friends you’ve made!",
             compatible: "Flower Hat Jelly, Fried Egg Jelly, Moon Jelly",
             incompatible: "Box Jelly, Elegant Jelly"
         },
         "Flower Hat Jelly": {
+            img: "assets/jellyfish/flower-hat.png",
             traits: "Eye-catching, Whimsical, Quirky",
             habitat: "You live near the ocean floor, your sprightly personality and striking looks standing out in the darkness. This stark difference causes some others to consider you odd, but your friends find that charm endearing.",
             compatible: "Elegant Jelly, Box Jelly, Black Sea Nettle",
             incompatible: "Man-O-War, Fried Egg Jelly"
         },
         "Spotted Jelly": {
+            img: "assets/jellyfish/spotted-jelly.png",
             traits: "Dependable, Caring, Trustworthy",
             habitat: "You live in bays, harbors and lagoons. Despite the hecticness of your life, living so close to land, others find that you’re a steady constant in their life, providing comfort in times of need.",
             compatible: "Box Jelly, Moon Jelly, Man-O-War",
             incompatible: "Elegant Jelly"
         },
         "Moon Jelly": {
+            img: "assets/jellyfish/moon-jelly.png",
             traits: "Quiet, Introspective, Peaceful",
             habitat: "You live in temperate seas, preferring consistent currents. This consistency allows for you to look inward, often lost in your own mind as the currents push you through the water, cradling you gently.",
             compatible: "Spotted Jelly, Elegant Jelly, Box Jelly",
             incompatible: "Man-O-War, Flower Hat Jelly"
         },
         "Elegant Jelly": {
+            img: "assets/jellyfish/elegant-jelly.png",
             traits: "Mysterious, Unknown, Misunderstood",
             habitat: "You live in various marine environments. Many find you hard to decipher, your anonymity causing some to mistrust you. Those who have managed to get close to you know that you simply enjoy your privacy.",
             compatible: "Moon Jelly, Flower Hat Jelly, Fried Egg Jelly",
             incompatible: "Black Sea Nettle, Spotted Jelly"
         },
         "Box Jelly": {
+            img: "assets/jellyfish/box-jelly.png",
             traits: "Strategic, Serious, Focused",
             habitat: "You prefer warm, tropical waters, using your sharp mind to propel you through labyrinths of coral reefs and sea life as you swim towards your goal. Some may call you single-minded, others say determined.",
             compatible: "Man-O-War, Moon Jelly, Spotted Jelly",
@@ -327,7 +335,9 @@ function quiz() {
                 resultjelly = jelly;
             }
         }
+
         document.getElementById("jelly").textContent = "You are a " + `${resultjelly}` + "!";
+        document.getElementById("jelly-pic").src = jellyinfo[resultjelly].img;
         document.getElementById("traits").textContent = "Traits: " + jellyinfo[resultjelly].traits;
         document.getElementById("habitat").textContent = jellyinfo[resultjelly].habitat;
         document.getElementById("compatible").textContent = "You're most compatible with " + jellyinfo[resultjelly].compatible;
